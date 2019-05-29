c4f6842fb679c31ec9b6201a93d9a55694b95118
Christopher Lemmer Webber <cwebber@dustycloud.org>
2019-05-29 15:59:17 -0400

    Add sudo'ability

--- hurd.rkt
+++ hurd.rkt
@@ -9,6 +9,21 @@
 
 (struct hurd (env ps))
 
+(define current-sudoer
+  (make-parameter #f))
+
+(define (call-with-sudoer sudoer thunk)
+  (parameterize ([current-sudoer sudoer])
+    (thunk)))
+
+(struct sudoable (sealed-by-cap sealed-by-sudo))
+
+(define (sudoable-seal cap val)
+  (define sudoer (current-sudoer))
+  (sudoable (cap-seal cap val)
+            (and sudoer
+                 (cap-seal sudoer val))))
+
 (define (hash-append ht k new-l)
   (define old-l
     (hash-ref ht k #f))
@@ -30,11 +45,27 @@
   (define sealed-vals
     (hash-ref env (cap-pred read-cap) empty))
   (map (lambda (sv)
-         (cap-unseal read-cap sv))
+         (cap-unseal read-cap
+                     (sudoable-sealed-by-cap sv)))
        sealed-vals))
 (define (env-read1 env read-cap d)
   (define vs (env-read env read-cap))
   (if (empty? vs) d (first vs)))
+(define (sudo-env-read env cap-pred sudoer)
+  (define sealed-vals
+    (hash-ref env cap-pred empty))
+  (map (lambda (sv)
+         (cap-unseal sudoer
+                     (sudoable-sealed-by-sudo sv)))
+       sealed-vals))
+(define (sudo-env-read1 env cap-pred sudoer d)
+  (define vs (sudo-env-read env cap-pred sudoer))
+  (if (empty? vs) d (first vs)))
+(define (sudo-env-all env sudoer)
+  (for/fold ([ht '#hasheq()])
+            ([cap-pred (hash-keys env)])
+    (hash-set ht cap-pred
+              (sudo-env-read env cap-pred sudoer))))
 
 (define (hasheq/l caps-and-vals)
   (let loop ([h (hasheq)]
@@ -44,7 +75,7 @@
        h]
       [(list-rest write-cap v caps-and-vals)
        (loop (hash-cons h (cap-pred write-cap)
-                        (cap-seal write-cap v))
+                        (sudoable-seal write-cap v))
              caps-and-vals)])))
 
 (define (hurd-write #:threads [ts null]
@@ -68,7 +99,7 @@
   (match-define (hurd env ps) w)
   (hurd (hash-set env (cap-pred write-cap)
                   (map (lambda (v)
-                         (cap-seal write-cap v))
+                         (sudoable-seal write-cap v))
                        vs))
         ps))
 (define (hurd-env-read w read-cap)
@@ -77,6 +108,15 @@
 (define (hurd-env-read1 w read-cap d)
   (match-define (hurd env ps) w)
   (env-read1 env read-cap d))
+(define (sudo-hurd-env-read w cap-pred sudoer)
+  (match-define (hurd env ps) w)
+  (sudo-env-read env cap-pred sudoer))
+(define (sudo-hurd-env-read1 w cap-pred sudoer d)
+  (match-define (hurd env ps) w)
+  (sudo-env-read1 env cap-pred sudoer d))
+(define (sudo-hurd-env-all w sudoer)
+  (match-define (hurd env ps) w)
+  (sudo-env-all env sudoer))
 
 (define (hurd-test env p)
   (os2-test env p))
@@ -117,4 +157,15 @@
        (listof any/c))]
   [hurd-env-read1
    (-> hurd? readable-cap? any/c
+       any/c)]
+  [call-with-sudoer
+   (-> writeable-cap? procedure? any/c)]
+  [sudo-hurd-env-read
+   (-> hurd? procedure? readable-cap?
+       (listof any/c))]
+  [sudo-hurd-env-read1
+   (-> hurd? procedure? readable-cap? any/c
+       any/c)]
+  [sudo-hurd-env-all
+   (-> hurd? readable-cap?
        any/c)]))
